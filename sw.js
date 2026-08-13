a0ef91cfa082e6d96555fd6015cdd43399fef75e
laurids28 <laurids.bauer@web.de>
2026-08-13 17:13:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='hyrox-v3';
+const C='hyrox-v4';
 const A=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting()));
